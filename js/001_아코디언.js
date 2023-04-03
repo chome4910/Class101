8a315c9ab4afb83eb765fd5ed09821b3cadfa876
chome4910 <76539716+chome4910@users.noreply.github.com>
2023-04-04 02:07:05 +0900

    Add files via upload

---
+++ js/001_아코디언.js
@@ -0,0 +1,28 @@
+html,body{
+    margin:0;
+
+}
+
+.panel-faq-container{
+
+}
+
+.panel-faq-title{
+
+}
+
+.panel-faq-answer{
+    display:none;
+}
+
+#btn_all-close{
+
+}
+
+#btn_all-close:hover{
+
+}
+
+.active{
+
+}
