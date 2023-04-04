21d3e96676d83672dff543909f211528d0fa44b9
chome4910 <76539716+chome4910@users.noreply.github.com>
2023-04-05 01:45:43 +0900

    Add files via upload

--- js/001_아코디언.js
+++ js/001_아코디언.js
@@ -1,28 +1,21 @@
-html,body{
-    margin:0;
+const panelFaqContainer = document.querySelectorAll( '.panel-faq-container' );
+let panelFaqAnswer = document.querySelectorAll( '.panel-faq-answer' );
+const btnAllClose = document.getElementById( 'btn-all-close' );
 
+
+for(let i = 0 ; i < panelFaqContainer.length ; i++){
+    panelFaqContainer[i].addEventListener('click',function(){
+        //클릭시 처리 할 일
+        //faq 제목 클릭시 본문이 보이게 active 클래스 추가해줘~
+        panelFaqAnswer[i].classList.add('active');                                                                                                                                                                                                                                                                                   
+    })
 }
 
-.panel-faq-container{
-
-}
-
-.panel-faq-title{
-
-}
-
-.panel-faq-answer{
-    display:none;
-}
-
-#btn_all-close{
-
-}
-
-#btn_all-close:hover{
-
-}
-
-.active{
-
-}
+btnAllClose.addEventListener( 'click', function() {
+    // 버튼 클릭시 처리할 일
+    for( let i=0; i < panelFaqAnswer.length; i++ ) {
+        panelFaqAnswer[i].classList.remove( 'active' );
+    }
+    
+} );
+    
